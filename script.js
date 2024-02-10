bfbacc42982439c47709028de5701698b6bd38e7
Kaique Moreto <kaique.moreto@gmail.com>
2024-02-10 19:57:05 -0300

    update js & add css

--- script.js
+++ script.js
@@ -95,6 +95,11 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
 //lOOP
 for (const item of perguntas) {
     const quizItem = template.content.cloneNode(true)
@@ -104,6 +109,19 @@ for (const item of perguntas) {
       const dt = quizItem.querySelector('dl dt').cloneNode(true)
       dt.querySelector('span').textContent = resposta
       dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+      dt.querySelector('input').value = item.respostas.indexOf(resposta)
+      dt.querySelector('input').onchange = (event) => {
+        const estaCorreta = event.target.value == item.correta
+        
+        corretas.delete(item)
+        if(estaCorreta) {
+            //alert('Acertou!')
+            corretas.add(item)
+        }
+        mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        //alert(corretas.size)
+      } 
+      
 
       quizItem.querySelector('dl').appendChild(dt)
     }
